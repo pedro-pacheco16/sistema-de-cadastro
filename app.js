b3062e58d1c0210088ca277f9371edbb22c99420
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-07-27 00:20:45 -0300

    adicionando mais linhas no app

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 console.log("rodando o sistema de gerenciamento de jogos")
+function categoriaPeso(peso) {
+    if (peso < 55) {
+        return "Peso leve";
+    } else if (peso < 65) {
+        return "Peso meio-médio";
+    } else if (peso < 75) {
+        return "Peso médio";
+    } else if (peso < 85) {
+        return "Peso meio-pesado";
+    } else {
+        return "Peso pesado";
+    }
+}
+
+function solucao(peso1, peso2) {
+    const categoriaPeso1 = categoriaPeso(peso1);
+    const categoriaPeso2 = categoriaPeso(peso2);
+
+    if (categoriaPeso1 === categoriaPeso2) {
+        return "PODEM LUTAR";
+    } else {
+        return "NAO PODEM LUTAR";
+    }
+}
+
+console.log(solucao(60, 64)); 
+console.log(solucao(55, 54));
